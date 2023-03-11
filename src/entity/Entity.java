782652122e10b8f0a7522562328ceb058a754f61
HoangNam <namtran0126@gmail.com>
2023-03-11 14:28:12 +0700

    collision detection

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public abstract class Entity {
@@ -13,4 +14,7 @@ public abstract class Entity {
 	
 	public int spriteCounter = 0;
 	public int spriteNum = 1;
+	
+	public Rectangle solidArea;
+	public boolean collisionOn = false;
 }
